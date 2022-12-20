14490d017ab9c7b311cc5e2f65bcb9e7cba1a6c9
Paulo Henrique <paulohenriqe226@gmail.com>
2022-12-19 23:17:43 -0300

    Seletor de abas OK-ps

---
+++ lib/Tabs/ChatTab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("ChatPane"),
+      ),
+    );
+  }
+}
---
+++ lib/Tabs/HealersTab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HealerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("HealersPane"),
+      ),
+    );
+  }
+}
